c934d462cc02f4b2f2144151e5dcc917fe3989b0
Hamza Elaasemy <hamza.elaasemy@imedia24.de>
2021-01-19 00:33:12 +0100

    Project stracture/layout

---
+++ src/components/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Main.module.scss';
+
+
+function Main() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.sidebar} >
+      </div>
+      <div className={styles.content} >
+      </div>
+    </div>
+  );
+}
+
+export default Main;
